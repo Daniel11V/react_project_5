4e1a07b1c31aed6e04d78920150ae53481145a97
Daniel Vinet <daniel1vinet@Gmail.com>
2021-09-07 20:19:57 -0300

    Visual Finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1 className="tittle">25 + 5 Clock</h1>
+      <div>
+        <div className="config inline">
+          <div id="break-label">Break Length</div>
+          <button id="break-decrement" onClick={{}}>
+            <i className="fa fa-arrow-down fa-2x"></i>
+          </button>
+          <div id="break-length" className="inline">5</div>
+          <button id="break-increment" onClick={{}}>
+            <i className="fa fa-arrow-up fa-2x"></i>
+          </button>
+        </div>
+        <div className="config inline">
+          <div id="session-label">Session Length</div>
+          <button id="session-decrement" onClick={{}}>
+            <i className="fa fa-arrow-down fa-2x"></i>
+          </button>
+          <div id="session-length" className="inline">5</div>
+          <button id="session-increment" onClick={{}}>
+            <i className="fa fa-arrow-up fa-2x"></i>
+          </button>
+        </div>
+      </div>
+      <div className="timer">
+        <div id="timer-label">
+          {(1) ? "Session" : "Break"}
+        </div>
+        <div id="time-left">
+          {`${"25"}:${"00"}`}
+        </div>
+      </div>
+      <div className="controler">
+        <button id="start_stop" onClick={{}}>
+          {
+            (1) ? <i className="fa fa-play fa-2x"></i>
+              : <i className="fa fa-pause fa-2x"></i>
+          }
+        </button>
+        <button id="reset" onClick={{}}>
+          <i className="fa fa-refresh fa-2x"></i>
+        </button>
+      </div>
+    </div >
   );
 }
 
